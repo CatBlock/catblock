6435fad97fd6598bbdb981d8a72cc43f69c6b366
Tomáš Taro <tomas@getadblock.com>
2016-03-21 10:27:11 +0100

    Update script for creating FF release

--- tools/ff_release.py
+++ tools/ff_release.py
@@ -8,8 +8,21 @@ License: GNU GPLv3
 
 import json # Provides JSON-related functions
 import os # Provides file-system functions
+import sys
+from distutils.dir_util import copy_tree # Provides function for copying trees
 
-os.chdir("..") # Move CWD to root of repo
+# Check, if we have got admin rights
+if os.getuid() != 0:
+    print "This script needs to be running with sudo privileges."
+    sys.exit(1)
+
+print "Preparing CatBlock for Firefox release..."
+
+os.chdir("..") # Move CWD to the root of repo
+
+origcwd = os.getcwd() # Save CWD for later use
+
+os.chdir("..") # Move CWD to the top of repo
 
 # Keys, which should NOT be deleted
 used = ["name", "version", "description", "content_scripts", "permissions", "manifest_version",
@@ -23,14 +36,16 @@ FF = {
     }
 }
 
-print "Preparing CatBlock for Firefox release..."
-with open("manifest.json", "rU") as chrome_manifest: # Opens the AdBlock i18n file in a secure manner for read-only
-    with open("ff_manifest.json", "w") as ff_manifest:
+with open(origcwd + "/manifest.json", "rU") as chrome_manifest: # Opens the CatBlock manifest file for read-only
+    if not os.path.exists(os.getcwd() + "/catblock_ff"): # If /catblock_ff folder doesn't exist, create it
+        os.makedirs(os.getcwd() + "/catblock_ff")
+    os.chdir(os.getcwd() + "/catblock_ff")
+    with open("manifest.json", "w") as ff_manifest:
         keys = json.load(chrome_manifest) # Creates a dict of all messages in the AdBlock file
         for key, value in keys.items():
             if key not in used:
                 print "Deleting key: %s" % key
-                del keys[key]
-        keys.update({ "applications": FF }) # Add FF specific key
-        ff_manifest.write(json.dumps(keys, sort_keys=True, indent=2, separators=(',', ':')))
+                del keys[key] # Delete keys, which shouldn't be included in FF manifest file
+        keys.update({ "applications": FF }) # Update FF manifest file with FF specific key
+        ff_manifest.write(json.dumps(keys, sort_keys=True, indent=2, separators=(',', ':'))) # Create FF manifest in JSON format
         print "CatBlock for Firefox has been built!"
