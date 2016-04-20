5bdc6d1de686d354a2fb581131bc1b878a7b8b0f
Tomáš Taro <tomas@getadblock.com>
2016-04-21 01:00:00 +0200

    Get rid of jQuery tabs

--- options/index.js
+++ options/index.js
@@ -2,7 +2,17 @@ function load_options() {
     // Check or uncheck each option.
     BGcall("get_settings", function(settings) {
         optionalSettings = settings;
-        var activeTab  = $.cookie('activetab');
+
+        // Hide settings
+        if (!optionalSettings.show_advanced_options) {
+            $(".advanced").hide();
+        }
+        if (SAFARI) {
+            $(".chrome-only").hide();
+        } else {
+            $(".safari-only").hide();
+        }
+
         if (window.location &&
             window.location.search) {
             var searchQuery = parseUri.parseSearch(window.location.search);
@@ -11,51 +21,24 @@ function load_options() {
                 activeTab = searchQuery.tab;
             }
         }
-        $("#tabpages").
-        tabs({
-            // Go to the last opened tab
-            active: activeTab,
-            activate: function(event, ui) {
-                $.cookie('activetab', ui.newTab.index(), {
-                    expires : 10
-                });
-            },
-            // Cache tabs
-            beforeLoad: function(event, ui) {
-                var tab_id = ui.tab.index();
-                if (ui.tab.data("loaded") && tab_id !== 3) {
-                    event.preventDefault();
-                    return;
-                }
-
-                ui.jqXHR.success(function() {
-                    ui.tab.data("loaded", true);
-                });
-            },
-            load: function(event, ui) {
-                //translation
-                localizePage();
-
-                // Toggle won't handle .advanced.chrome-only
-                if (!optionalSettings.show_advanced_options)
-                    $(".advanced").hide();
-                if (SAFARI)
-                    $(".chrome-only").hide();
-                if (!SAFARI)
-                    $(".safari-only").hide();
-
-                // Must load tab .js here: CSP won't let injected html inject <script>
-                // see index.html:data-scripts
-                ui.tab["0"].dataset.scripts.split(' ').forEach(function(scriptToLoad) {
-                    // CSP blocks eval, which $().append(scriptTag) uses
-                    var s = document.createElement("script");
-                    s.src = scriptToLoad;
-                    document.body.appendChild(s);
-                });
-            },
-        }).
-        show();
     });
+
+    // Load different tabs
+    $("nav > a").click(function(event) {
+        var target = event.target;
+        var scripts = target.dataset.scripts;
+        var page = target.dataset.page;
+        console.log(scripts);
+        $("#content").load(page, localizePage);
+        scripts.split(" ").forEach(function(scriptToLoad) {
+            // CSP blocks eval, which $().append(scriptTag) uses
+            var s = document.createElement("script");
+            s.src = scriptToLoad;
+            document.body.appendChild(s);
+        });
+    });
+
+    $("nav > a:first-child").click(); // Show General tab
 }
 
 var language = navigator.language.match(/^[a-z]+/i)[0];
@@ -181,5 +164,5 @@ $(document).ready(function(){
     showMiniMenu();
     displayVersionNumber();
     displayTranslationCredit();
-    localizePage();
+    //localizePage();
 })
