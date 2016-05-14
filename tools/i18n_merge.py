e8b14ebe902f68205977b710d9745a6485fd5eed
Tomáš Taro <tomas@getadblock.com>
2016-05-14 13:27:19 +0200

    Automatize the language processing workflow

--- tools/i18n_merge.py
+++ tools/i18n_merge.py
@@ -1,28 +1,82 @@
+#!/usr/bin/python
+
 """
+Script for managing CatBlock translations in Chrome JSON format
+
+It is intended for:
+- downloading CatBlock language files from Crowdin
+- merging them with AdBlock language files
 Merges i18n files from ../_locales/ and ../catblock/_locales
 
-Author: Kieran Peckett
+Author: Kieran Peckett, Tomas Taro
 License: GNU GPLv3
 """
 
 import json # Provides JSON-related functions
 import os # Provides file-system functions
+import urllib
+import zipfile
+import shutil
 
 os.chdir("..") # Move CWD to root of repo
 
-locales = os.listdir(r"catblock/_locales") # Load a list of all locales in the CatBlock locales folder.
-for locale in locales: # "locale" below is the locale selected for this loop
-	print "Merging i18n file for locale: " + locale
-	with open("_locales/" + locale + "/messages.json", "rU") as ab_read: # Opens the AdBlock i18n file in a secure manner for read-only
-		with open("catblock/_locales/" + locale + "/messages.json", "rU") as cb_file: # Opens the CatBlock i18n file in a secure manner
-			ab_messages = json.load(ab_read) # Creates a dict of all messages in the AdBlock file
-			cb_messages = json.load(cb_file) # Creates a dict of all messgaes in the CatBlock file
-			ab_messages.update(cb_messages) # Updates the AdBlock strings with the CatBlock ones
-			# Closes CatBlock messages file
-		# Closes AdBlock messages file
-	with open("_locales/" + locale + "/messages.json", "w") as ab_write: # Open the AdBlock i18n file as write-only (will overwite data!)
-		ab_write.write(json.dumps(ab_messages, sort_keys=True, indent=2, separators=(',', ':'))) # Writes the JSON data to the AdBlock file in an easy-to-read format
-		# Closes AdBlock messages file
-	print "Merged i18n file for locale: %s" % locale
+API_KEY = open(os.getcwd() + "/.crowdin_key").read()
+API_URL = "https://api.crowdin.com/api/project/catblock"
+
+EXPORT_URL = API_URL + "/export?key=" + API_KEY + "&json=true"
+DOWNLOAD_URL = API_URL + "/download/all.zip?key=" + API_KEY
+
+# Build project languages
+print "Building project's language messages"
+
+req = urllib.urlopen(EXPORT_URL).read()
+data = json.loads(req)
+
+status = data["success"]["status"]
+
+if status == "built":
+    print " - Project has been built successfully"
+elif status == "skipped":
+    print " - Build has been skipped, previous is up-to-date or less than 30 minutes old"
+
+# Download languages packed in a .zip file
+print "Downloading .zip file containing language messages"
+
+req = urllib.urlretrieve(DOWNLOAD_URL, "translations.zip")
+
+# Extract them
+print " - Extracting languages"
+
+zip = zipfile.ZipFile("translations.zip")
+zip.extractall(os.getcwd() + "/catblock/_locales")
+
+print " - Removing .zip file"
+os.remove(os.getcwd() + "/translations.zip")
+
+
+# Merging languages
+print "Merging language messages"
+ab_locales = os.listdir(r"_locales") # Load a list of all locales in the AdBlock locales folder
+
+for locale in ab_locales: # "locale" below is the locale selected for this loop
+    print "Merging i18n file for locale: " + locale
+    with open("_locales/" + locale + "/messages.json", "rU") as ab_read: # Opens the AdBlock i18n file in a secure manner for read-only
+        # If we don't have CatBlock's specific language file, then merge CatBlock's english language file into AdBlock's language file
+        try:
+            with open("catblock/_locales/" + locale + "/messages.json", "rU") as cb_file: # Opens the CatBlock i18n file in a secure manner
+                ab_messages = json.load(ab_read) # Creates a dict of all messages in the AdBlock file
+                cb_messages = json.load(cb_file) # Creates a dict of all messgaes in the CatBlock file
+                ab_messages.update(cb_messages) # Updates the AdBlock strings with the CatBlock ones
+            # Closes CatBlock messages file
+        except:
+            with open("catblock/_locales/" + "en" + "/messages.json", "rU") as cb_file: # Opens the CatBlock i18n file in a secure manner
+                ab_messages = json.load(ab_read) # Creates a dict of all messages in the AdBlock file
+                cb_messages = json.load(cb_file) # Creates a dict of all messgaes in the CatBlock file
+                ab_messages.update(cb_messages) # Updates the AdBlock strings with the CatBlock ones
+            # Closes AdBlock messages file
+        with open("_locales/" + locale + "/messages.json", "w") as ab_write: # Open the AdBlock i18n file as write-only (will overwite data!)
+            ab_write.write(json.dumps(ab_messages, sort_keys=False, indent=2, separators=(',', ':'))) # Writes the JSON data to the AdBlock file in an easy-to-read format
+        # Closes AdBlock messages file
+    print "Merged i18n file for locale: %s" % locale
 
 # Done!
--- tools/update_l10n.pl
+++ tools/update_l10n.pl
@@ -13,9 +13,6 @@
 use strict;
 use warnings;
 
-# not tested on older versions of Perl
-use 5.16.2;
-
 use utf8;
 use Encode;
 use FindBin;
@@ -53,10 +50,10 @@ sub export_locales {
         open my $file, "<", $api_key_file;
         my $key = <$file>;
         close $file;
-        
+
         # call API
         my $status = get($api_export_url.$key);
-        
+
         # try to figure out what happened
         if (!defined $status){
             # could be internet issue, server issue, or wrong API key.
@@ -86,15 +83,15 @@ sub get_locales {
     # make sure we don't already have other temporary files
     rmtree($tmp_dir);
     mkdir($tmp_dir);
-    
+
     # download locales from Crowdin
     # if getting a 500 or SSL error, try installing/updating Mozilla::CA
     my $status = getstore($crowdin_url,"$tmp_dir/adblock.zip");
     die "\nError downloading $crowdin_url: $status\n" unless is_success($status);
-    
+
     # use command line tools instead of perl modules to extract the archive
     $Archive::Extract::PREFER_BIN = 1;
-    
+
     # unzip the archive
     my $ae = Archive::Extract->new(archive => "$tmp_dir/adblock.zip");
     $ae->extract(to => $tmp_dir) or die "\n".$ae->error;
@@ -103,12 +100,12 @@ sub get_locales {
 sub dir_listing {
     # take path to a folder as input
     my $path = shift(@_);
-    
+
     opendir(FOLDER,$path)
         or die "\nCan't open directory $path: $!\n";
     my @listing = readdir(FOLDER);
     closedir(FOLDER);
-    
+
     # return the array of folders
     return @listing;
 }
@@ -116,11 +113,11 @@ sub dir_listing {
 sub get_json {
     # take path to a file as input
     my $file = shift(@_);
-    
+
     # read and convert the file into a hash
     my $data = read_entire_file($file);
     my $json = decode_json(encode('utf8',$data));
-    
+
     # return the hash
     return $json;
 }
@@ -128,14 +125,14 @@ sub get_json {
 sub check_new_locales {
     # take project we're working on as input
     my $project = shift(@_);
-    
+
     # grab lists of new locales and json for source locale
     my @new_locales = dir_listing("$tmp_dir/$project/");
     my $json = get_json("$tmp_dir/$project/$src_file");
-    
+
     # total number of strings in source json
     my $src_strings_count = keys($json);
-    
+
     my @adblock_caps;
     foreach my $string (keys($json)){
         # find and remember strings where AdBlock is capitalized differently
@@ -143,12 +140,12 @@ sub check_new_locales {
             push(@adblock_caps,$string);
         }
     }
-    
+
     foreach my $language (@new_locales){
         if (($language =~ /^[a-z]{2}(_[A-Z]{2})?$/) && (-s "$tmp_dir/$project/$language/messages.json")){
             # read new file for this language
             my $strings_new = get_json("$tmp_dir/$project/$language/messages.json");
-            
+
             # don't use translation if it's less than 60% done
             my $tr_strings_count = keys($strings_new);
             if ($tr_strings_count <= $src_strings_count * 0.6){
@@ -158,11 +155,11 @@ sub check_new_locales {
                 rmtree("$tmp_dir/$project/$language");
                 next;
             }
-            
+
             unless (-s $locale_dirs{$project}.$language."/messages.json"){
                 print colored("!", 'red'), " $language: new translation added, has the translator been added to the credits?\n";
             }
-            
+
             foreach my $string (keys($strings_new)){
                 # check if "AdBlock" is capitalized correctly
                 if (!grep(/^${string}$/, @adblock_caps)){
@@ -170,7 +167,7 @@ sub check_new_locales {
                         print colored("!", 'red'), " $language: AdBlock capitalized incorrectly in $string\n";
                     }
                 }
-                
+
                 # check if strings contain the right placeholders
                 if ($strings_new->{$string}{placeholders}){
                     foreach my $placeholder (keys($strings_new->{$string}{placeholders})){
@@ -180,7 +177,7 @@ sub check_new_locales {
                     }
                 }
             }
-            
+
             # Chrome Web Store descriptions shouldn't exceed 132 characters
             if ($strings_new->{description2}{message}){
                 if (length($strings_new->{description2}{message}) > 132){
@@ -189,20 +186,20 @@ sub check_new_locales {
             }
         }
     }
-    
+
     return 1;
 }
 
 sub read_lines_file {
     # take path to a file as input
     my $file = shift(@_);
-    
+
     # read file
     open(FILE,"<:encoding(utf8)",$file)
         or die "\nCan't open file $file: $!\n";
     my @lines = <FILE>;
     close(FILE);
-    
+
     # output array of lines in file
     return @lines;
 }
@@ -210,10 +207,10 @@ sub read_lines_file {
 sub read_entire_file {
     # take path to a file as input
     my $file = shift(@_);
-    
+
     local $/; # enable slurp mode
     my @contents = read_lines_file($file);
-    
+
     # output whole content of the file
     return $contents[0];
 }
@@ -221,14 +218,14 @@ sub read_entire_file {
 sub strings_in_use {
     # take list of files to search in as input
     my @files = @_;
-    
+
     my @html_strings;
     my @js_strings;
-    
+
     foreach my $file (@files){
         # store lines of the file in array
         my @lines = read_lines_file($file);
-        
+
         # find all occurences of strings being used in the line
         if ($file =~ /\.html$/){
             foreach my $line (@lines){
@@ -244,12 +241,12 @@ sub strings_in_use {
             }
         }
     }
-    
+
     # combine strings found in html files and js files
     my @allstrings = (@html_strings,@js_strings);
     # make sure we don't have duplicates and sort them
     my @stringsinuse = sort(uniq(@allstrings));
-    
+
     # output all strings found in code
     return @stringsinuse;
 }
@@ -257,9 +254,9 @@ sub strings_in_use {
 sub html_js_files {
     # take paths to folders to search as input
     my @folders = @_;
-    
+
     my @files;
-    
+
     foreach my $folder (@folders){
         # search for html and js files and store list
         find(sub{
@@ -268,7 +265,7 @@ sub html_js_files {
             push(@files,$File::Find::name);
         },$folder);
     }
-    
+
     # return list of files found
     return @files;
 }
@@ -278,9 +275,9 @@ sub missing_unused {
     my $project = shift(@_);
     my $file = "$tmp_dir/$project/$src_file";
     my $json = get_json($file);
-    
+
     my @files, my @strings;
-    
+
     # find html/js files for the relevant project
     if ($project eq "contributors"){
         @files = html_js_files("$locale_dirs{$project}/../../");
@@ -292,7 +289,7 @@ sub missing_unused {
             # don't look in directories that are separate projects
             my $file = $_; # current filename
             return $File::Find::prune = 1 if (any {$file eq $_} @{["contributors", "installed", "pay"]});
-            
+
             return unless -f;
             return unless (/\.html$/ || /\.js$/);
             push(@files,$File::Find::name);
@@ -300,10 +297,10 @@ sub missing_unused {
     } else {
         @files = html_js_files($root);
     }
-    
+
     # find all the strings that are used in the html/js files
     @strings = strings_in_use(@files);
-    
+
     # get includes from include/exclude file
     my @includes, my @excludes;
     if (-e $includefile){
@@ -315,7 +312,7 @@ sub missing_unused {
             }
         }
     }
-    
+
     # find strings that need to be removed
     my @unused;
     foreach my $string (keys($json)){
@@ -324,7 +321,7 @@ sub missing_unused {
             push(@unused,$string);
         }
     }
-    
+
     # find strings that are missing from the json
     my @missing;
     foreach my $string (@strings){
@@ -332,12 +329,12 @@ sub missing_unused {
             push(@missing,$string)
         }
     }
-    
+
     my %unused_missing = (
         unused => [@unused],
         missing => [@missing]
     );
-    
+
     # returns a hash of unused and missing strings
     return %unused_missing;
 }
@@ -345,21 +342,21 @@ sub missing_unused {
 sub print_unused_missing {
     # get a hash of unused and missing strings as input
     my %strings = @_;
-    
+
     my $unusedcount = @{$strings{unused}};
     my $missingcount = @{$strings{missing}};
-    
+
     # print unused/missing strings, if any
     if ($unusedcount > 0 || $missingcount > 0){
         print "\n";
-        
+
         if (@{$strings{unused}}){
             print colored("!", 'red'), " Found $unusedcount UNUSED strings:\n";
             foreach my $string (@{$strings{unused}}){
                 print "  - $string\n";
             }
         }
-        
+
         if (@{$strings{missing}}){
             print colored("!", 'red'), " Found $missingcount ", colored("MISSING", 'bold'), " strings:\n";
             foreach my $string (@{$strings{missing}}){
@@ -369,17 +366,17 @@ sub print_unused_missing {
     } else {
         print "none found.\n"
     }
-    
+
     return 1;
 }
 
 sub rename_folder_structure {
     # take path to the root folder as input
     my $localesroot = shift(@_);
-    
+
     # grab a list of the locale folders
     my @localeslist = dir_listing($localesroot);
-    
+
     # rename folders for languages that don't have several variants
     # e.g. en_US becomes en, unless there are other variants like en_GB
     foreach my $folder (@localeslist){
@@ -391,12 +388,12 @@ sub rename_folder_structure {
             }
         }
     }
-    
+
     # apparently Chrome absolutely wants Norwegian to be nb
     if (-d $localesroot."no"){
         rename($localesroot."no",$localesroot."nb");
     }
-    
+
     return 1;
 }
 
@@ -404,50 +401,50 @@ sub main {
     # disable output buffering
     STDERR->autoflush(1);
     STDOUT->autoflush(1);
-    
+
     # correctly display UTF-8 characters
     binmode(STDOUT, ":utf8");
-    
+
     print "> Building Crowdin archive... ";
     export_locales();
-    
+
     print "> Downloading and extracting locales... ";
     get_locales();
     print "done.\n";
-    
+
     foreach my $project (keys(%locale_dirs)){
         if (-d $locale_dirs{$project}){
             if ($project ne "adblock"){
                 print colored("> Working on $project", 'bold'), "\n";
             }
-        
+
             print "> Renaming Crowdin directory structure... ";
             rename_folder_structure("$tmp_dir/$project/");
             print "done.\n";
-    
+
             print "> Checking new locales... \n";
             check_new_locales($project);
-        
+
             print "> Looking for unused or missing strings... ";
             my %strings = missing_unused($project);
             print_unused_missing(%strings);
-    
+
             print "> Replacing previous locales... ";
             rmtree($locale_dirs{$project});
             dirmove("$tmp_dir/$project/",$locale_dirs{$project})
                 or die "\nCan't copy new locales: $!\n";
             print "done.\n";
-            
+
             if ($project ne "adblock"){
                 print colored("> Finished $project", 'bold'), "\n";
             }
         }
     }
-    
+
     print "> Cleaning up... ";
     rmtree($tmp_dir);
     print "done.\n";
-    
+
     return 1;
 }
 
