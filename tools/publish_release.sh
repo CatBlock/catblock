cbdce14ebcbb337d2bc921a5cb5ee9fddca874ea
Travis <travis@travis-ci.org>
2016-05-25 16:25:16 +0200

    Update post-release script

--- tools/publish_release.sh
+++ tools/publish_release.sh
@@ -1,17 +1,31 @@
-if [ "$TRAVIS_PULL_REQUEST" == "false" && "$TRAVIS_BRANCH" == "master" ]; then
-  echo -e "Starting to update test\n"
+echo -e "Starting to update builds on GitHub"
 
-  #go to home and setup git
-  git config --global user.email "travis@travis-ci.org"
-  git config --global user.name "Travis"
+# Go to the root of the repo
+cd ..
 
-  #using token clone master branch
-  git clone --quiet --branch=master https://${GH_TOKEN}@github.com/CatBlock/catblock.git  master > /dev/null
+# Create /builds folder in the root
+mkdir ~/builds
 
-  #add, commit and push files
-  git add -f .
-  git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to test branch"
-  git push -fq origin test > /dev/null
+# Copy builds to the /builds folder
+cp builds/* ~/builds
 
-  echo -e "Done updating builds on GitHub."
-fi
+# Go to the root folder
+cd
+
+# Setup Git
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "Travis"
+
+# Using token clone packages branch
+git clone --branch=packages https://${GH_TOKEN}@github.com/CatBlock/catblock.git
+
+# Add, commit and push files
+cd catblock/
+
+cp ~/builds builds/*
+
+git add -f .
+git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to the 'packages' branch"
+git push origin HEAD
+
+echo -e "Done updating builds on GitHub."
