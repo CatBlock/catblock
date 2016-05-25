3c8cef045697374f6a4a349130260414a8107649
Tomáš Taro <tomas@getadblock.com>
2016-05-25 20:12:33 +0200

    Update build script

--- tools/publish_release.sh
+++ tools/publish_release.sh
@@ -7,14 +7,14 @@ git config --global user.email "tomas@getadblock.com"
 git config --global user.name "Tomáš Taro"
 
 git stash save -u
-git checkout packages
+git checkout -b packages
 
 rm -rf builds/
 mkdir builds
 
 git stash pop
 
-git add -f .
+git add builds/
 
 git commit -am "Travis build $TRAVIS_BUILD_NUMBER pushed to the 'packages' branch"
 git push
