d8a81ee16d04ada7b3f867eaf45dbb315fa053a1
Tomáš Taro <tomas@getadblock.com>
2016-05-25 17:54:34 +0200

    Update Travis.yml file

--- tools/publish_release.sh
+++ tools/publish_release.sh
@@ -3,10 +3,6 @@ echo -e "Starting to update builds on GitHub"
 # Go to the root of the repo
 cd ..
 
-# Setup Git
-git config --global user.email "travis@travis-ci.org"
-git config --global user.name "Travis"
-
 git add -f .
 git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to the 'packages' branch"
 git push origin packages
