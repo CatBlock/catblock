4c6565127456daa8ec1e72d385ab2cf91596d73e
Tomáš Taro <tomas@getadblock.com>
2016-05-25 20:06:55 +0200

    Update publish script

--- tools/publish_release.sh
+++ tools/publish_release.sh
@@ -3,8 +3,20 @@ echo -e "Starting to update builds on GitHub"
 # Go to the root of the repo
 cd ..
 
+git config --global user.email "tomas@getadblock.com"
+git config --global user.name "Tomáš Taro"
+
+git stash save -u
+git checkout packages
+
+rm -rf builds/
+mkdir builds
+
+git stash pop
+
 git add -f .
-git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to the 'packages' branch"
-git push origin packages
+
+git commit -am "Travis build $TRAVIS_BUILD_NUMBER pushed to the 'packages' branch"
+git push
 
 echo -e "Done updating builds on GitHub."
