16452f88abc8e06bcfb5acbdd674d83761469ba4
Tomáš Taro <tomas@getadblock.com>
2016-05-25 20:49:00 +0200

    Update publishing script

--- tools/publish_release.sh
+++ tools/publish_release.sh
@@ -3,7 +3,9 @@ echo -e "Starting to update packages on GitHub"
 # Go to the root of the repo
 cd ..
 
-git clone https://github.com/CatBlock/catblock-nightlies.git
+cd ..
+
+git clone https://${GH_TOKEN}@github.com/CatBlock/catblock-nightlies.git
 
 cp -a /catblock/builds/. /catblock-nightlies/
 
