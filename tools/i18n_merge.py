9d4eb7097195173b4cce5d279f53741a99ee305c
Tomáš Taro <tomas@getadblock.com>
2016-09-02 15:50:30 +0200

    Run .py scripts only when they haven't been imported as a modules
    Signed-off-by: Tomáš Taro <tomas@getadblock.com>

--- tools/i18n_merge.py
+++ tools/i18n_merge.py
@@ -11,75 +11,81 @@ Author: Kieran Peckett, Tomas Taro
 License: GNU GPLv3
 """
 
-import json # Provides JSON-related functions
-import os # Provides file-system functions
-import urllib.request # Provides functions related to HTTP communication
-import zipfile # Provides functions related to zip files
+def main():
 
-os.chdir("..") # Move CWD to root of repo
+    import json # Provides JSON-related functions
+    import os # Provides file-system functions
+    import urllib.request # Provides functions related to HTTP communication
+    import zipfile # Provides functions related to zip files
 
-API_KEY = open(os.getenv("HOME") + "/.crowdin_key").read() # Read the API key from file
+    os.chdir("..") # Move CWD to root of repo
 
-if (API_KEY == ""):
-    print("A Crowdin API key hasn't been found, exiting script")
-    quit()
+    API_KEY = open(os.getenv("HOME") + "/.crowdin_key").read() # Read the API key from file
 
-API_URL = "https://api.crowdin.com/api/project/catblock"
+    if (API_KEY == ""):
+        print("A Crowdin API key hasn't been found, exiting script")
+        quit()
 
-EXPORT_URL = API_URL + "/export?key=" + API_KEY + "&json=true"
-DOWNLOAD_URL = API_URL + "/download/all.zip?key=" + API_KEY
+    API_URL = "https://api.crowdin.com/api/project/catblock"
 
-# Build project languages
-print("Building project's language messages:")
+    EXPORT_URL = API_URL + "/export?key=" + API_KEY + "&json=true"
+    DOWNLOAD_URL = API_URL + "/download/all.zip?key=" + API_KEY
 
-req = urllib.request.urlopen(EXPORT_URL).read()
-data = json.loads(req.decode("utf-8"))
+    # Build project languages
+    print("Building project's language messages:")
 
-status = data["success"]["status"]
+    req = urllib.request.urlopen(EXPORT_URL).read()
+    data = json.loads(req.decode("utf-8"))
 
-if status == "built":
-    print(" - Project has been built successfully")
-elif status == "skipped":
-    print(" - Build has been skipped, previous is up-to-date or less than 30 minutes old")
+    status = data["success"]["status"]
 
-# Download languages packed in a .zip file
-print("Downloading .zip file containing language messages:")
+    if status == "built":
+        print(" - Project has been built successfully")
+    elif status == "skipped":
+        print(" - Build has been skipped, previous is up-to-date or less than 30 minutes old")
 
-req = urllib.request.urlretrieve(DOWNLOAD_URL, "translations.zip")
+    # Download languages packed in a .zip file
+    print("Downloading .zip file containing language messages:")
 
-# Extract them
-print(" - Extracting languages")
+    req = urllib.request.urlretrieve(DOWNLOAD_URL, "translations.zip")
 
-zip = zipfile.ZipFile("translations.zip")
-zip.extractall(os.getcwd() + "/catblock/_locales")
+    # Extract them
+    print(" - Extracting languages")
 
-print(" - Removing .zip file")
-os.remove(os.getcwd() + "/translations.zip")
+    zip = zipfile.ZipFile("translations.zip")
+    zip.extractall(os.getcwd() + "/catblock/_locales")
 
-# Merging languages
-print("Merging language messages:")
+    print(" - Removing .zip file")
+    os.remove(os.getcwd() + "/translations.zip")
 
-ab_locales = os.listdir(r"_locales") # Load a list of all locales in the AdBlock locales folder
+    # Merging languages
+    print("Merging language messages:")
 
-for locale in ab_locales: # "locale" below is the locale selected for this loop
-    print (" - Merging i18n file for locale: " + locale)
-    with open("_locales/" + locale + "/messages.json", "rU") as ab_read: # Opens the AdBlock i18n file in a secure manner for read-only
-        # If we don't have CatBlock's specific language file, then merge CatBlock's english language file into AdBlock's language file
-        try:
-            with open("catblock/_locales/" + locale + "/messages.json", "rU") as cb_file: # Opens the CatBlock i18n file in a secure manner
-                ab_messages = json.load(ab_read) # Creates a dict of all messages in the AdBlock file
-                cb_messages = json.load(cb_file) # Creates a dict of all messgaes in the CatBlock file
-                ab_messages.update(cb_messages) # Updates the AdBlock strings with the CatBlock ones
-            # Closes CatBlock messages file
-        except:
-            with open("catblock/_locales/" + "en" + "/messages.json", "rU") as cb_file: # Opens the CatBlock i18n file in a secure manner
-                ab_messages = json.load(ab_read) # Creates a dict of all messages in the AdBlock file
-                cb_messages = json.load(cb_file) # Creates a dict of all messgaes in the CatBlock file
-                ab_messages.update(cb_messages) # Updates the AdBlock strings with the CatBlock ones
+    ab_locales = os.listdir(r"_locales") # Load a list of all locales in the AdBlock locales folder
+
+    for locale in ab_locales: # "locale" below is the locale selected for this loop
+        print (" - Merging i18n file for locale: " + locale)
+        with open("_locales/" + locale + "/messages.json", "rU") as ab_read: # Opens the AdBlock i18n file in a secure manner for read-only
+            # If we don't have CatBlock's specific language file, then merge CatBlock's english language file into AdBlock's language file
+            try:
+                with open("catblock/_locales/" + locale + "/messages.json", "rU") as cb_file: # Opens the CatBlock i18n file in a secure manner
+                    ab_messages = json.load(ab_read) # Creates a dict of all messages in the AdBlock file
+                    cb_messages = json.load(cb_file) # Creates a dict of all messgaes in the CatBlock file
+                    ab_messages.update(cb_messages) # Updates the AdBlock strings with the CatBlock ones
+                # Closes CatBlock messages file
+            except:
+                with open("catblock/_locales/" + "en" + "/messages.json", "rU") as cb_file: # Opens the CatBlock i18n file in a secure manner
+                    ab_messages = json.load(ab_read) # Creates a dict of all messages in the AdBlock file
+                    cb_messages = json.load(cb_file) # Creates a dict of all messgaes in the CatBlock file
+                    ab_messages.update(cb_messages) # Updates the AdBlock strings with the CatBlock ones
+                # Closes AdBlock messages file
+            with open("_locales/" + locale + "/messages.json", "w") as ab_write: # Open the AdBlock i18n file as write-only (will overwite data!)
+                ab_write.write(json.dumps(ab_messages, ensure_ascii=False, sort_keys=False, indent=2, separators=(',', ':'))) # Writes the JSON data to the AdBlock file in an easy-to-read format
             # Closes AdBlock messages file
-        with open("_locales/" + locale + "/messages.json", "w") as ab_write: # Open the AdBlock i18n file as write-only (will overwite data!)
-            ab_write.write(json.dumps(ab_messages, ensure_ascii=False, sort_keys=False, indent=2, separators=(',', ':'))) # Writes the JSON data to the AdBlock file in an easy-to-read format
-        # Closes AdBlock messages file
-    print (" - Merged i18n file for locale: %s" % locale)
+        print (" - Merged i18n file for locale: %s" % locale)
 
-# Done!
+    # Done!
+
+# Execute this script only, if it hasn't been imported as a module
+if __name__ == "__main__":
+    main()
