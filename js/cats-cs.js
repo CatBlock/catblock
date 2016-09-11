d3352c60cdf64f89cdf3a99389419c7474a5ea7f
Tomáš Taro <tomas@getadblock.com>
2016-09-11 02:03:00 +0200

    Remove localstorage key after running a content-script
    Signed-off-by: Tomáš Taro <tomas@getadblock.com>

--- js/cats-cs.js
+++ js/cats-cs.js
@@ -1,11 +1,15 @@
 // Run "CATS" detection code just on the dedicated page
 // cez execute script
 (function() {
-    var shouldEnableCats = localStorage.getItem("project-cats");
+    var key = "project-cats";
+    var shouldEnableCats = localStorage.getItem(key);
 
     if (shouldEnableCats) {
         chrome.runtime.sendMessage({ command: "enableprojectcats" });
     } else {
         chrome.runtime.sendMessage({ command: "disableprojectcats" });
     }
+
+    // Remove the key from a local storage
+    localStorage.removeItem(key);
 })();
