c708fc0ec2f327da488b9722534b35b848fff5cf
Tomáš Taro <tomas@getadblock.com>
2017-01-21 18:40:45 +0100

    Merge branch 'master' into remove-eval
    Signed-off-by: Tomáš Taro <tomas@getadblock.com>

--- js/bandaids.js
+++ js/bandaids.js
@@ -1,9 +1,10 @@
-// Youtube-related code in this file based on code (c) Adblock Plus. GPLv3.
-// and https://hg.adblockplus.org/adblockpluschrome/file/aed8fd38e824/safari/include.youtube.js
+// Youtube-related code in this file based on © Adblock Plus™ code licensed under GPLv3.
 function run_bandaids() {
     // Tests to determine whether a particular bandaid should be applied
     var apply_bandaid_for = "";
-    if (/mail\.live\.com/.test(document.location.hostname)) {
+    if (/facebook\.com/.test(document.location.hostname)) {
+        apply_bandaid_for = "facebook";
+    } else if (/mail\.live\.com/.test(document.location.hostname)) {
         apply_bandaid_for = "hotmail";
     } else if (/mobilmania\.cz|zive\.cz|doupe\.cz|e15\.cz|sportrevue\.cz|autorevue\.cz/.test(document.location.hostname)) {
         apply_bandaid_for = "czech_sites";
@@ -31,7 +32,7 @@ function run_bandaids() {
             css_chunk.type = "text/css";
             (document.head || document.documentElement).insertBefore(css_chunk, null);
             css_chunk.sheet.insertRule(".WithRightRail { right: 0 !important; }", 0);
-            css_chunk.sheet.insertRule("#RightRailContainer  { display:none !important; visibility: none !important; orphans: 4321 !important; }" , 0);
+            css_chunk.sheet.insertRule("#RightRailContainer  { display: none !important; visibility: hidden !important; orphans: 4321 !important; }" , 0);
         },
         czech_sites: function() {
             var player = document.getElementsByClassName("flowplayer");
@@ -41,6 +42,104 @@ function run_bandaids() {
                     player[i].removeAttribute("data-ad");
                 }
             }
+        },
+        facebook: function() {
+            // Block sidebar ads
+            var css_chunk = document.createElement("style");
+            css_chunk.type = "text/css";
+            (document.head || document.documentElement).insertBefore(css_chunk, null);
+            css_chunk.sheet.insertRule("#pagelet_ego_pane .uiHeader.uiHeaderTopBorder.mbs.uiSideHeader, #pagelet_ego_pane .ego_unit { display:none !important; visibility: hidden !important; orphans: 4321 !important; }", 0);
+
+            // Block news-feed ads
+            // The following code is based on:
+            // https://greasyfork.org/en/scripts/22210-facebook-unsponsored
+            var streamSelector = "div[id^='topnews_main_stream']";
+            var storySelector = "div[id^='hyperfeed_story_id']";
+            var sponsoredSelectors = [
+                "a[href^='https://www.facebook.com/about/ads']",
+                "a[href^='https://www.facebook.com/ads/about']",
+                "a[href^='/about/ads']",
+                "a[href^='https://i.facebook.com/']",
+                "a[href*='hc_ref=ADS']",
+                "a[href^='/ads/about']"
+            ];
+
+            var suggestedPhrases = [
+                "Popular Live Video",
+                "Suggested Post"
+            ];
+
+            var stream;
+            var mutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+
+            function block(story) {
+                if (!story) {
+                    return;
+                }
+                story.remove();
+            }
+
+            function check(story) {
+                if (!story) {
+                    return;
+                }
+
+                var sponsored = false;
+                var i;
+                for (i=0; i < sponsoredSelectors.length; i++) {
+                    if (story.querySelectorAll(sponsoredSelectors[i]).length) {
+                        sponsored = true;
+                        break;
+                    }
+                }
+
+                var divs = [];
+                var j;
+                if (!sponsored) {
+                    divs = story.querySelectorAll(".userContentWrapper > div > div");
+                    for (i=0; i < divs.length; i++) {
+                        for (j=0; j < suggestedPhrases.length; j++) {
+                            if (divs[i].innerText.trim() === suggestedPhrases[j]) {
+                                sponsored = true;
+                                break;
+                            }
+                        }
+
+                        if (sponsored) {
+                            break;
+                        }
+                    }
+                }
+
+                if (sponsored) {
+                    block(story);
+                }
+            }
+
+            function process() {
+                // Locate the stream every iteration to allow for FB SPA navigation which
+                // replaces the stream element
+                stream = document.querySelector(streamSelector);
+                if (!stream) {
+                    return;
+                }
+
+                var stories = stream.querySelectorAll(storySelector);
+                if (!stories.length) {
+                    return;
+                }
+
+                var i;
+                for (i=0; i < stories.length; i++) {
+                    check(stories[i]);
+                }
+            }
+
+            var observer = new mutationObserver(process);
+            observer.observe(document.querySelector("body"), {
+                "childList": true,
+                "subtree": true
+            });
         }
     }; // end bandaids
 
