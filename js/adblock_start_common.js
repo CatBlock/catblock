2e903a413db16be0464d1d621866d89bacc5b127
Tomáš Taro <tomas@getadblock.com>
2017-01-30 21:37:38 +0100

    Move Safari-specific functions from ab_start_common.js to ab_start_safari.js
    Signed-off-by: Tomáš Taro <tomas@getadblock.com>

--- js/adblock_start_common.js
+++ js/adblock_start_common.js
@@ -2,65 +2,6 @@
 // so that web sites can not clobber the default implementation
 var abConfirm = window.confirm;
 
-// Return the ElementType element type of the given element.
-function typeForElement(el) {
-    // TODO: handle background images that aren't just the BODY.
-    switch (el.nodeName.toUpperCase()) {
-        case "IMG":
-        case "INPUT":
-        case "PICTURE": return ElementTypes.image;
-        case "AUDIO":
-        case "VIDEO": return ElementTypes.media;
-        case "SCRIPT": return ElementTypes.script;
-        case "FRAME":
-        case "IFRAME": return ElementTypes.subdocument;
-        case "OBJECT":
-        case "EMBED": return ElementTypes.object;
-        case "LINK":
-            // favicons are reported as 'other' by onBeforeRequest.
-            // if this is changed, we should update this too.
-            if (/(^|\s)icon($|\s)/i.test(el.rel)) {
-                return ElementTypes.other;
-            }
-            return ElementTypes.stylesheet;
-        case "BODY": return ElementTypes.image;
-        default: return ElementTypes.NONE;
-    }
-}
-
-// If url is relative, convert to absolute.
-function relativeToAbsoluteUrl(url) {
-    // Author: Tom Joseph of AdThwart
-
-    if (!url) {
-        return url;
-    }
-
-    // If URL is already absolute, don't mess with it
-    if (/^[a-zA-Z\-]+\:/.test(url)) {
-        return url;
-    }
-
-    if (url[0] === "/") {
-        // Leading // means only the protocol is missing
-        if (url[1] && url[1] === "/") {
-            return document.location.protocol + url;
-        }
-
-        // Leading / means absolute path
-        return document.location.protocol + "//" + document.location.host + url;
-    }
-
-    // Remove filename and add relative URL to it
-    var base = document.baseURI.match(/.+\//);
-
-    if (!base) {
-        return document.baseURI + "/" + url;
-    }
-
-    return base[0] + url;
-}
-
 // Do not make the frame display a white area
 // Not calling .remove(); as this causes some sites to reload continuesly
 function removeFrame(el) {
