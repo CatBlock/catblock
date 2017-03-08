3f7587db9c78cc2a5537a37551e96e49effbb764
Tomáš Taro <tomas@getadblock.com>
2017-03-08 01:05:58 +0100

    Implement WebSocket blocking
    Signed-off-by: Tomáš Taro <tomas@getadblock.com>

--- filtering/filteroptions.js
+++ filtering/filteroptions.js
@@ -14,14 +14,15 @@ var ElementTypes = {
     xmlhttprequest: 512,
     "document": 1024,
     elemhide: 2048,
-    popup: 4096
+    popup: 4096,
+    websocket: 32768
     // If you add something here, update .DEFAULTTYPES and .CHROMEONLY below.
 };
 // The types that are implied by a filter that doesn't explicitly specify types
 ElementTypes.DEFAULTTYPES = 1023;
 // Add here any types that Safari does not support.
 ElementTypes.CHROMEONLY = (ElementTypes.object_subrequest | ElementTypes.other |
-                           ElementTypes.xmlhttprequest);
+                           ElementTypes.xmlhttprequest | ElementTypes.websocket);
 
 // Convert a webRequest.onBeforeRequest type to an ElementType.
 ElementTypes.fromOnBeforeRequestType = function(type) {
